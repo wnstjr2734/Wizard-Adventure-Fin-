56f4603179cba94869b7f20f301c6bb98bd91352
ZeroFe <skel2cha@gmail.com>
2022-08-18 23:14:31 +0900

    차지 효과 구현

--- Assets/02_Script/HitObject/ChargeEffect.cs
+++ Assets/02_Script/HitObject/ChargeEffect.cs
@@ -12,25 +12,74 @@ public class ChargeEffect : MonoBehaviour
 {
     // 차지 이펙트는 3단계로 구성
     // 1단계(처음 켰을 때) - 
+    private static readonly int ringIndex = 1;
+    private static readonly int glowIndex = 2;
 
     [SerializeField, Tooltip("단계별 차지 정도")]
     private Vector3 chargeScale = new Vector3(0.2f, 0.5f, 1.0f);
 
-    private ParticleSystem effect;
-    [SerializeField, Tooltip("2단계 Ring Particle")]
-    private ParticleSystem ringEffect;
-    [SerializeField, Tooltip("3단계 Glow Particle")]
-    private ParticleSystem glowEffect;
+    private ParticleSystem[] particleSystems;
+    private Light light;
+
+    [SerializeField, Tooltip("불, 얼음, 전기 속성 색상")] 
+    private Color[] elementColor = new Color[] {Color.red, Color.cyan, Color.yellow};
+
+    [SerializeField, Tooltip("차지에 필요한 시간")]
+    private float chargeTime = 5.0f;
+
+    private void Awake()
+    {
+        particleSystems = GetComponentsInChildren<ParticleSystem>();
+        light = GetComponentInChildren<Light>();
+        print("particleSystems count : " + particleSystems.Length);
+    }
 
     private void OnEnable()
     {
         transform.localScale = Vector3.one * chargeScale.x;
-        ringEffect.gameObject.SetActive(false);
-        glowEffect.gameObject.SetActive(false);
+        particleSystems[ringIndex].gameObject.SetActive(false);
+        particleSystems[glowIndex].gameObject.SetActive(false);
+        SetColor(PlayerMagic.ElementType.Fire);
+        StartCoroutine(nameof(IEChargeGauge));
+    }
+
+    private IEnumerator IEChargeGauge()
+    {
+        float chargePercent = 0.0f;
+        float chargeSpeed = 1.0f / chargeTime;
+
+        while (chargePercent < 0.5f)
+        {
+            chargePercent += Time.deltaTime * chargeSpeed;
+            yield return null;
+        }
+        particleSystems[ringIndex].gameObject.SetActive(true);
+        transform.DOScale(Vector3.one * chargeScale.y, 0.5f);
+
+        while (chargePercent < 1.0f)
+        {
+            chargePercent += Time.deltaTime * chargeSpeed;
+            yield return null;
+        }
+        particleSystems[glowIndex].gameObject.SetActive(true);
+        transform.DOScale(Vector3.one * chargeScale.z, 0.5f);
     }
 
     private void OnDisable()
     {
         
+    }
+
+    public void SetColor(PlayerMagic.ElementType element)
+    {
+        Color particleColor = elementColor[(int) element];
+
+        foreach (var system in particleSystems)
+        {
+            var main = system.main;
+            main.startColor = particleColor;
+        }
+
+        light.color = particleColor;
     }
 }
