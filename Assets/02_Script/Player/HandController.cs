a2756ed4ef16e960a510ffc4fd0d0cb5ba688c8e
KimDoYoung <rlaehdudv@gmail.com>
2022-09-02 22:56:37 +0900

    Player UI VR 컨트롤러 조작 테스트

--- Assets/02_Script/Player/HandController.cs
+++ Assets/02_Script/Player/HandController.cs
@@ -31,6 +31,49 @@ public class HandController : MonoBehaviour
     [SerializeField, Tooltip("오른손 애니메이션 컨트롤러")]
     private Animator rightHandAnimator;
 
+    [Header("플레이어 UI 조작")]
+    [SerializeField] private LineRenderer line;  
+    [SerializeField] private Transform basePos;    
+    [SerializeField] private float maxDis = 30f;
+    Ray ray = new Ray();
+    
+
+    private RaycastHit hit;
+
+    private void Start()
+    {
+        ray.origin = basePos.localPosition;
+        ray.direction = basePos.forward;        
+    }
+
+    private void Update()
+    {
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+            Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red);
+            print("레이 시작 위치 : " +ray.origin);
+            print("기준점 위치 : " + basePos.position);
+            line.SetPosition(0, ray.origin);
+            line.SetPosition(1, hit.point);
+            
+        }
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+        {
+            line.gameObject.SetActive(true);
+            print("버튼 클릭");
+            
+        }
+        else if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+        {
+            print("버튼 실행");
+            line.gameObject.SetActive(false);
+        }
+
+    }
+
+
+
     // 애니메이션 스트링 해쉬 코드
     private readonly int animNumHash = Animator.StringToHash("AnimNum");
 
