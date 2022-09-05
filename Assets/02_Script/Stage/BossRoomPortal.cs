0bf28f1ad76d0ee87d940acdc35247a7523cf7a6
ZeroFe <skel2cha@gmail.com>
2022-09-06 00:24:37 +0900

    포탈 오류 안 생기게 수정 및 상속 가능하게 변경 최종 맵 추가 아웃트로 기능 및 보스 포탈 추가

---
+++ Assets/02_Script/Stage/BossRoomPortal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 보스 포탈 스크립트
+/// 작성자 - 차영철
+/// </summary>
+public class BossRoomPortal : Portal
+{
+    [SerializeField, Tooltip("아웃트로 영상")] 
+    private GameObject outro;
+
+    protected override void OnUsePortal()
+    {
+        // 아웃트로 틀기
+        outro.SetActive(true);
+    }
+}
